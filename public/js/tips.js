200a114b8600ddea942a94492b6957a987c9c509
tjuacalla123 <>
2021-03-12 23:59:22 -0800

    no special characters in plastic name

--- public/js/tips.js
+++ public/js/tips.js
@@ -4,7 +4,7 @@ $( document ).ready(function() {
        descDiv.hide(); 
         var titleDiv = $(this).children('.tip-titles'); 
         $(this).click(function(e) {
-             descDiv.toggle();
+             descDiv.toggle("fast");
         });
     });
 });
